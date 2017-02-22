845fd761900e8e54cc8524d30f54d3b446d1ed0e
Matthew McCord <mattm.eternal@gmail.com>
2017-02-22 12:47:33 -0600

    fixture, products view, products view test

--- bangazonsite/initial_site/admin.py
+++ bangazonsite/initial_site/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from initial_site import models
 
-# Register your models here.
+@admin.register(models.Customer, models.ProductType, models.Product, models.PaymentType, models.Order)
+
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    pass
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+class PaymentTypeAdmin(admin.ModelAdmin):
+    pass
+
+class OrderAdmin(admin.ModelAdmin):
+    pass
--- bangazonsite/initial_site/tests.py
+++ bangazonsite/initial_site/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+from django.test.utils import setup_test_environment
+from initial_site import models
 
-# Create your tests here.
+'''
+Unit Tests for the views
+'''
+
+setup_test_environment()
+
+class ProductViewTests(TestCase):
+
+    '''
+    This class will test the views relating to Products
+    '''
+
+    def test_can_list_all_products(self):
+        """
+        Testing if we can list all products on a view
+        """
+        ball = models.Product("1", "1", 2, "Baseball", 33.33, 444)
+        ball.save()
+        
+        client = Client()
+        response = client.get(reverse('products'))
+        
+        self.assertQuerysetEqual(response.context['object_list'], ['<Product: Baseball>'])
