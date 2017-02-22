6f13396daca99ee54208ac9b5d2afde1e88d4f0b
Paul Martin <Dmart331@gmail.com>
2017-02-22 10:10:09 -0600

    Created test for Customer

--- bangazonsite/initial_site/tests.py
+++ bangazonsite/initial_site/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-
+from .models import Customer
 # Create your tests here.
+
+class CustomerTest(TestCase):
+	def test_is_a_customer(self):
+		
+		david = Customer("123 Front St", "456 Back St","Smyrna", "Tennssee", "37167")
+		self.assertIsInstance(david, Customer)
